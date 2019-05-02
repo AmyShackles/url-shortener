aaacfb1e9e657b833b546abeeeb2f43bab826556
Amy Shackles <amyshackles@gmail.com>
2019-05-01 20:12:07 -0700

    Adding client

--- server/index.js
+++ server/index.js
@@ -19,6 +19,16 @@ mongoose.connect(mongoURI, connectOptions, (err, db) => {
     if (err) console.log(`Error: ${er}`);
     console.log(`Connected to MongoDB`);
 });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-type, Accept, x-access-token, X-Key");
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+    } else {
+        next();
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
