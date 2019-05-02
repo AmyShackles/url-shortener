c867b23896bbcacf784870e1e9381b27e054fdd2
Amy Shackles <amyshackles@gmail.com>
2019-05-02 09:31:03 -0700

    Changed error url

--- server/routes/urlshorten.js
+++ server/routes/urlshorten.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const validUrl = require("valid-url");
 const UrlShorten = mongoose.model("UrlShorten");
 const shortid = require("shortid");
-const errorUrl = "http://localhost/error";
+const errorUrl = process.env.NODE_ENV === 'development' ? "http://localhost/error" : "https://hackless.herokuapp.com/error";
 
 
 module.exports = app => {
