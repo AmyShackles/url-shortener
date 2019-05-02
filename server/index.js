7128bcc8ca5b8a3ee78c40ea5ae29c1e7bceff4e
Amy Shackles <amyshackles@gmail.com>
2019-05-02 07:42:02 -0700

    Attempt to add React endpoint as allowed host

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ mongoose.connect(mongoURI, connectOptions, (err, db) => {
 });
 app.use(bodyParser.json());   
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.REACT_ENDPOINT);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Content-type, Accept, x-access-token, X-Key");
     if (req.method === "OPTIONS") {
