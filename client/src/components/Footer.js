c85e9b758d7838026a3a8a14f28b10b556ac6401
Amy Shackles <amyshackles@gmail.com>
2019-05-02 23:03:45 -0700

    Made a component for the footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Footer.css";
+
+export function Footer() {
+    return (
+        <footer role="contentinfo">
+            <p>
+                Amy Shackles © 2019
+            </p>
+        </footer>
+    )
+}
+
