ddd25e3577e9050671e9048db31513ad2ab7d71c
Amy Shackles <amyshackles@gmail.com>
2019-05-02 23:02:45 -0700

    Added a contact page

---
+++ client/src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <h2>I can be reached via email at <a href="mailto:amyshackles@gmail.com">amyshackles@gmail.com</a></h2>
+            <h2>If you'd like to figure out what I'm currently up to, you can follow me on <a href="https://github.com/amyshackles">Github</a>, <a href="https://linkedin.com/in/amyshackles">LinkedIn</a>, and/or <a href="https://twitter.com/amyshackles">Twitter</a></h2>
+        </div>
+    )
+}
