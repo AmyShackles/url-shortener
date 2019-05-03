ff3bb7f90bfbddd7ea63b3b1513bf663d539cfeb
Amy Shackles <amyshackles@gmail.com>
2019-05-02 23:05:17 -0700

    Made an index file to export components for easier importing

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+import { About } from "./About.js";
+import { Contact } from "./Contact.js";
+import { Footer } from "./Footer.js";
+import { Header } from "./Header.js";
+import { Navigation } from "./Navigation.js";
+import { URLShortener } from "./URLShortener.js";
+
+export { About, Contact, Footer, Header, Navigation, URLShortener };
