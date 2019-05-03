59a446b1303e9d9daff740f6036c8d23abee62c2
Amy Shackles <amyshackles@gmail.com>
2019-05-02 23:03:58 -0700

    Made a component for the header

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../styles/Header.css";
+
+export function Header() {
+    return (
+        <header role="banner">
+            <p>Hackless!</p>
+        </header>
+    )
+}
